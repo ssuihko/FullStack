061efc463ed6fccf85949d8cb005892979823083
Sini Suihkonen <sahsuihkonen@gmail.com>
2020-02-18 21:29:19 +0200

    P4.1

---
+++ FullStack4/index.js
@@ -0,0 +1,189 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const Person = require('./models/person')
+
+morgan.token('body', function (req) { return JSON.stringify(req.body) })
+
+app.use(express.static('build'))
+app.use(express.json())
+
+const url = process.env.MONGODB_URI
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name ===  'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
+
+// app.use(morgan('tiny'))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+app.use(cors())
+app.use(bodyParser.json())
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+let persons = [
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
+
+  }, {
+    name: 'Ada Lovelace',
+    number: '39-44-534356767',
+    id: 4
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234567',
+    id: 3
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: 2
+  }
+
+]
+
+var length = persons.length.toString()
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+  Person.find({}).then(persons => {
+    res.json(persons.map(person => person.toJSON()))
+  })
+})
+
+app.get('/info', (req, res) => {
+  res.send('<p>Phonebook has info for ' + length + ' people ' + '<br>' + new Date() + ' </p>')
+})
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+    // const id = Number(request.params.id)
+    // persons = persons.filter(person => person.id !== id)
+
+  // length = persons.length.toString()
+
+// response.status(204).end()
+})
+
+app.get('/api/persons/:id', (req, res, next) => {
+
+  Person.findById(req.params.id)
+    .then(person => {
+      if(person) {
+        res.json(person.toJSON())
+      } else {
+        res.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+
+})
+
+app.put('api/persons/:id', (req, res, next) => {
+
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person)
+    .then(updatedPerson => {
+      res.json(updatedPerson.toJSON())
+    })
+    .catch(error => next(error))
+
+})
+
+app.get('/api/blogs', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+app.post('/api/persons', (request, response) => {
+
+  let nameArray = persons.map(( { name }) => name)
+
+  const body = request.body
+
+  if (!body.name || !body.number ) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * Math.floor(1000)),
+  })
+
+  if(nameArray.indexOf(person.name) > -1) {
+
+    return response.status(400).json({
+      error: 'name must be unique, do you want to replace the number recorded to this name'
+    })
+  }
+
+  persons = persons.concat(person)
+  nameArray = nameArray.concat(person.name)
+
+  length = persons.length.toString()
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson.toJSON())
+  })
+})
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ FullStack4/models/person.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+mongoose.set('useCreateIndex', true)
+const uniqueValidator = require('mongoose-unique-validator')
+require('dotenv').config()
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)  })
+
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+  }
+})
+
+personSchema.plugin(uniqueValidator)
+
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Person', personSchema)
