75cb32c566b010a8dba60d1e1991d21e3f0d03f0
Sini Suihkonen <sahsuihkonen@gmail.com>
2020-10-19 00:13:49 +0300

    fixes to part2 countries

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -5,16 +5,10 @@ import './App.css';
 const App = () => {
 
   const [ countries, setCountries] = useState([])
-  const [ filtering, setFilter ] = useState(true)
   const [ filteredCountries, setFilteredCountries ] = useState('')
   const [ weather, setWeather ] = useState([])
   const [ location, setLocation ] = useState([])
   const api_key = process.env.REACT_APP_API_KEY
-  const numMatch = countries.filter(c => c.name.toLowerCase().includes(filteredCountries.toLowerCase())).length
-
-  var countriesShow = filtering
-  ? countries
-  : countries.filter(c => c.name.toLowerCase().includes(filteredCountries.toLowerCase()))
 
   useEffect(() => {
     axios.get('https://restcountries.eu/rest/v2/all')
@@ -23,8 +17,11 @@ const App = () => {
     })
   }, [])
 
-function WeatherQuery(country ) {
+  const handleFilter = (event) => {
+    setFilteredCountries(event.target.value)
+  } 
 
+function WeatherQuery(country) {
   const params = {
     access_key: api_key,
     query: country.capital
@@ -47,15 +44,6 @@ function WeatherQuery(country ) {
 
 }
 
-  const handleFilter = (event) => {
-    setFilteredCountries(event.target.value)
-    if(event.target.value === '') {
-      setFilter(true)
-    } else {
-      setFilter(false)
-    }
-  } 
-
   const Country = ( { country } ) => {
 
     if(weather.length === 0 || (location.name !== country.capital && location.country !== null)  ) {
@@ -65,46 +53,47 @@ function WeatherQuery(country ) {
     return (
       <div>
            <h2>{country.name} </h2>
-              <p> capital {country.capital }</p>
-              <p> population {country.population}</p>
+            <p> capital {country.capital }</p>
+            <p> population {country.population}</p>
             <h3>Languages</h3>
             <ul>
               { country.languages.map( c => 
                 <li key={c.name}>{c.name}</li>
               )}
             </ul>
-
             <img src={country.flag} alt="flag" style={{ width: "200px", height:"150px" }}></img>
             <p>Weather in {country.capital}</p>
-             <p>Temperature : { weather.temperature } Celcius</p>
+            <p>Temperature : { weather.temperature } Celcius</p>
             <img src={ weather.weather_icons } alt="wicon" style={{ width: "200px", height:"150px" }}></img>
-              <p>Wind : { weather.wind_speed } mph direction {weather.wind_dir} </p>
+            <p>Wind : { weather.wind_speed } mph direction {weather.wind_dir} </p>
       </div>      
     )
   }
 
+  const Show = () => {
 
-  const Show = ( {country, amount } ) => {
+    const matches = countries.filter(c => c.name.toLowerCase().includes(filteredCountries.toLowerCase())).length
+    var countriesToShow = countries.filter(c => c.name.toLowerCase().includes(filteredCountries.toLowerCase()))
 
-    if(amount >= 11 && numMatch !== 250) {
+    if(matches >= 11 && matches !== 250) {
       return (
         <div>Too many matches, specify another filter</div>
       )
     }
   
-    if( amount < 11 && amount > 0 && amount !== 1 ) {
+    if( matches < 11 && matches > 0 && matches !== 1 ) {
 
     return ( 
-      country.map(country => 
+      countriesToShow.map(country => 
       <div key={country.name}>{country.name}
       <button key={country.name} onClick={() => { setFilteredCountries(country.name) ; WeatherQuery(country) } }>Show</button>
       </div>
         ) 
     )
 
-    } else if(amount === 1) {
+    } else if(matches === 1) {
      return (
-       <Country country={country[0]}></Country>
+       <Country country={countriesToShow[0]}></Country>
      )
         
      } else {
@@ -112,11 +101,10 @@ function WeatherQuery(country ) {
      }
   }
 
-      
     return (
       <div>
         find countries <input value={filteredCountries} onChange={handleFilter} />
-        <Show country={countriesShow} amount={numMatch} />
+        <Show />
       </div>
     )
   }
